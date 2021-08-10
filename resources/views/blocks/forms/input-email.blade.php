fbab36377a87d85013f283c38684816bede140f8
David Cova <davidcova88@gmail.com>
2021-08-10 12:11:38 +0100

    Update input-email.blade.php

--- resources/views/blocks/forms/input-email.blade.php
+++ resources/views/blocks/forms/input-email.blade.php
@@ -8,14 +8,17 @@
     @endphp
 
     <label class="pt-2" for="{{$identifier}}">
-        <div class="text-capitalize">{{$label ?? $identifier}}
+        <div class="text-capitalize {{$label_classes ?? ''}}">{{(($label != false) ? $label : '') ?? $identifier}}
             @if (isset($required))
             <span class="text-p mx-2">*</span>
             @endif
         </div>
     </label>
-    <input type="email" name="{{$identifier}}" id="{{$identifier}}" class="form-control rounded-0 @error($identifier) is-invalid @enderror"
-        value="{{old($identifier, $val)}}" {{$required ?? ''}}>
+    <input type="email" name="{{$identifier}}" id="{{$identifier}}" class="form-control {{$input_classes ?? ''}} rounded-0 @error($identifier) is-invalid @enderror"
+        value="{{old($identifier, $val)}}"
+        {{$required ?? ''}}
+        placeholder="{{$placeholder ?? ''}}"
+        >
     @error($identifier)
     <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
     @enderror
