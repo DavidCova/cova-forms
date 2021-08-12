a31c36446c443a0192b052c5ccd121d95ad29b59
David Cova <davidcova88@gmail.com>
2021-08-12 16:18:42 +0100

    Update input-email.blade.php

--- resources/views/blocks/forms/input-email.blade.php
+++ resources/views/blocks/forms/input-email.blade.php
@@ -8,7 +8,7 @@
     @endphp
     @if ($label != false)
     <label class="pt-2" for="{{$identifier}}">
-        <div class="text-capitalize {{$label_classes ?? ''}}">{{(($label != false) ? $label : '') ?? $identifier}}
+        <div class="text-capitalize {{$label_classes ?? ''}}">{{(($label != false) ? $label : '') ?? str_replace("_"," ",$identifier)}}
             @if (isset($required))
             <span class="text-p mx-2">*</span>
             @endif
