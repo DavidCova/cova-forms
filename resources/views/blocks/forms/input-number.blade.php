95b7660892cdaa3a35b474de2c3e13dddf131f3a
David Cova <davidcova88@gmail.com>
2021-08-12 16:18:59 +0100

    Update input-number.blade.php

--- resources/views/blocks/forms/input-number.blade.php
+++ resources/views/blocks/forms/input-number.blade.php
@@ -7,7 +7,7 @@
     }
     @endphp 
     
-    <label class="pt-2" for="{{$identifier}}"><div class="text-capitalize">{{$label ?? $identifier}}
+    <label class="pt-2" for="{{$identifier}}"><div class="text-capitalize">{{$label ?? str_replace("_"," ",$identifier)}}
         @if (isset($required))
         <span class='text-p mx-2'>*</span>
         @endif    
