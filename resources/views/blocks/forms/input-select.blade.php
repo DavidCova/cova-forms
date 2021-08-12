c53f400d17be6d0efc48717806105c58cdf5f09d
David Cova <davidcova88@gmail.com>
2021-08-12 16:19:24 +0100

    Update input-select.blade.php

--- resources/views/blocks/forms/input-select.blade.php
+++ resources/views/blocks/forms/input-select.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group">
     <label class="pt-2" for="{{$identifier}}">
-        <div class="text-capitalize">{{__($label ?? $identifier)}}
+        <div class="text-capitalize">{{__($label ?? str_replace("_"," ",$identifier))}}
             @if (isset($required))
             <span class='text-p mx-2'>*</span>
             @endif    
