480021f78fbd3afd8716d61f82676b8688399f8c
David Cova <davidcova88@gmail.com>
2021-08-23 17:39:30 +0100

    Update input-textarea.blade.php

--- resources/views/blocks/forms/input-textarea.blade.php
+++ resources/views/blocks/forms/input-textarea.blade.php
@@ -9,17 +9,14 @@
     <label class="pt-2" for="{{$identifier}}">
         <div class="text-capitalize">{{$label ?? str_replace("_"," ",$identifier)}}
             @if (isset($required))
-            @if ($required == 1)
+            @if ($required == true)
             <span class="text-p mx-2">*</span>
             @endif
             @endif
         </div>
     </label>
     <textarea name="{{$identifier}}" id="{{$identifier}}"
-    @if (isset($required))
-        @if ($required==1) required
-        @endif
-    @endif
+    {{isset($required) ? ($required == true) ? 'required' :'' : ''}}
         class="form-control {{$classes ?? ''}} @error($identifier) is-invalid @enderror" cols="{{$cols}}" rows="{{$rows}}" style="{{$style ?? ''}}">{{old($identifier,$val)}}</textarea>
     @error($identifier)
     <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
