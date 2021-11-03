e19549836724ad0ea178baa63a038528d6cb8704
DavidCova <davidcova88@gmail.com>
2021-11-03 18:49:31 +0000

    update

--- src/Providers/FormServiceProvider.php
+++ src/Providers/FormServiceProvider.php
@@ -9,7 +9,7 @@ class FormServiceProvider extends ServiceProvider
     public function boot()
     {        
         $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'cova-forms');
-        $this->publishes([__DIR__ . '/../../app/Http/Livewire' => app_path('views/vendor/cova-forms')], 'cova-livewire');
+        $this->publishes([__DIR__ . '/../../app/Http/Livewire' => app_path('Http/Livewire/')], 'cova-livewire');
         $this->publishes([__DIR__ . '/../../resources/views' => resource_path('views/vendor/cova-forms')], 'cova-forms');
     }
 
