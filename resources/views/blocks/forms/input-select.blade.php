f196c9e1e5cfa055b8e21661f0e25d4ad14b0ed5
David Cova <davidcova88@gmail.com>
2021-11-11 17:04:51 +0000

    Update input-select.blade.php

--- resources/views/blocks/forms/input-select.blade.php
+++ resources/views/blocks/forms/input-select.blade.php
@@ -1,9 +1,11 @@
 <div class="form-group">
+    @if (isset($label)) 
     <label class="pt-2" for="{{$identifier}}">
         <div class="text-capitalize">{{__($label ?? str_replace("_"," ",$identifier))}}
             @if (isset($required))<span class='text-p mx-2'>*</span>@endif    
         </div>
     </label>
+    @endif
     <select name="{{$identifier}}" id="{{$identifier}}" {{$readonly ?? ''}}
         class="form-select {{$input_classes ?? ''}} rounded-0 @error($identifier) is-invalid @enderror"
         @if(isset($model))
@@ -14,32 +16,38 @@
         @endif
         {{$required ?? ''}}>
         <option value="">{{__('Select')}}</option>
-       @if ((is_object($options) || is_array($options)))
-        @foreach ($options as $o)
-        <option value="@if(isset($key)){{$o->$key}}@else{{$o}}@endif"
-            @if(isset($current))
-                @if (isset($key))
-                    @if($o->$key == $current)
-                    selected
+        @if ((is_object($options) || is_array($options)))
+            @foreach ($options as $o)
+                <option value="@if(isset($key)){{$o->$key}}@else{{$o}}@endif"
+                    @if(isset($current))
+                        @if (isset($key))
+                            @if($o->$key == $current)
+                                selected
+                            @endif
+                        @if (isset($value))
+                            @elseif ($value == $current)
+                                selected
+                            @endif
+                        @elseif($o == $current)
+                            selected
+                        @endif
+                    @endif            
+                    >
+                    @if(!isset($value))
+                        {{$o}}
+                    @else
+                        {{$o[$value]}}
                     @endif
-
-                @elseif ($option == $current)
-                selected
-                @endif
-            @endif            
-            >
-            @if(isset($option)){{$o->$option}}@else{{$option}}@endif
-        </option>
-        @endforeach
+                </option>
+            @endforeach
         @else
-        @foreach (json_decode($options,true) as $data)
-        <option value="{{$data['id']}}">{{$data['name']}}</option>
-        @endforeach
-       @endif
-       
+            @foreach (json_decode($options,true) as $data)
+                <option value="{{$data['id']}}">{{$data['name']}}</option>
+            @endforeach
+        @endif       
     </select>
     @error($identifier)
-    <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
+        <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
     @enderror
 </div>
 @push('scripts')
